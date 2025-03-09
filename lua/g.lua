5a6c6db61601185cff1478b56c497cc9ed1ad69b
evheath <elliot@businessfactory.dev>
2025-03-09 18:09:40 -0500

    starting to modularize

---
+++ lua/g.lua
@@ -0,0 +1,12 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- disabling netrw in leu of nvim tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
